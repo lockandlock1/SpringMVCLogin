61c7296e1d0c54c5797a8ce3ee7e20dfe3a095ba
redkyu <hongstar2311@gmail.com>
2022-02-25 16:24:27 +0900

    Initial commit

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,19 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+//        member.s
+    }
+}
