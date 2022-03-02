65c4fa214895d39abeab14fb88e1149d32129e83
redkyu <hongstar2311@gmail.com>
2022-03-02 21:31:37 +0900

    1. 쿠키만 이용한 로그인 2. Session 직접 만들어 로그인 3. HttpSession 활용한 로그인

--- src/main/java/hello/login/domain/member/MemberRepository.java
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -3,8 +3,8 @@ package hello.login.domain.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.swing.plaf.metal.MetalMenuBarUI;
+import java.util.*;
 
 @Slf4j
 @Repository
@@ -14,6 +14,35 @@ public class MemberRepository {
     private static long sequence = 0L;
 
     public Member save(Member member) {
-//        member.s
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+//        List<Member> all = findAll();
+//        for (Member m : all) {
+//            if(m.getLoginId().equals(loginId)){
+//                return Optional.of(m);
+//            }
+//        }
+//
+//        return Optional.empty();
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
